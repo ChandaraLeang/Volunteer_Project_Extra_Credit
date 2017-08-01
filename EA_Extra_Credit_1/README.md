a8fe8d460e7e93546c342f059e99b1dd18612d4b
ChandaraLeang <leangchandara@gmail.com>
2017-07-31 20:36:09 -0500

    Add EA Volunteer Project Extra Credit

---
+++ EA_Extra_Credit_1/README.md
@@ -0,0 +1,9 @@
+# EA_Extra_Credit_1
+
+Name: CHANDARA LEANG
+ID: 108619
+
+# Framework
+Hibernate
+
+# JUnit Test
---
+++ EA_Extra_Credit_1/src/main/java/cs544/mum/edu/model/Status.java
@@ -0,0 +1,15 @@
+package cs544.mum.edu.model;
+
+/**
+ * Name: CHANDARA LEANG
+ * ID: 108619
+ * Task: Extra Credit 1
+ * Enum: Status
+ */
+
+public enum Status {
+	DONE, 
+	INPROGRESS, 
+	PENDDING, 
+	NEW
+}
